60333dd842c60a8d1db3963f5c30393d2925b09d
Donglai Duan <donglaiduann@gmail.com>
2025-03-16 22:48:20 -0500

    fix tailwind, begin navigation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import React, { useState, useRef, useEffect, Children } from "react";
 const Home = () => {
+    const navigate = useNavigate();
     const onClick = () => {
         console.log("hello") // to navigation
+        navigate(`/about`);
     }
 
     return (
         <div><h1 className="">hello world</h1>
-        <button onClick={onClick} className="w-11 h-10"></button></div>
+        <button onClick={onClick} className="w-11 h-10 bg-blue-500"></button></div>
     );
 };
 
