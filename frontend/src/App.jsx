5e87da36b4209d2160ad7dda46fc0da5eacea0d2
Donglai Duan <donglaiduann@gmail.com>
2025-04-28 15:45:47 -0500

    cleanup and fonts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Layout from "./layouts/Layout.jsx";
 import About from "./pages/About.jsx";
 import AddYours from "./pages/AddYours.jsx";
 import Map from "./pages/Map.jsx";
+import "./App.css";
+
 const LoadingSpinner = () => (
   <div className="flex justify-center items-center min-h-screen">
     <div className="loading loading-spinner loading-lg text-primary"></div>
