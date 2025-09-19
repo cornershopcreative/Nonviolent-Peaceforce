8456fc42e1595d76ea2f8e89a441670b84e6980f
Benjamin Eisenberg <benjamin@cornershopcreative.com>
2025-09-19 19:52:05 +0200

    update ENV vars

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -13,10 +13,10 @@ const About = () => {
     e.preventDefault();
     emailjs
       .send(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         formData,
-        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       )
 
       .then(
